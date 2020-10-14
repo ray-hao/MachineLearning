838c19304168896acf7cb0c42beacdcc91a2063e
Ray Hao <71276800+ray-hao@users.noreply.github.com>
2020-10-13 20:48:30 -0400

    Delete costFunction.m

--- costFunction.m
+++
@@ -1,8 +0,0 @@
-function cost = costFunction(X, y, theta, lambda)
-
-m = length(y);
-prediction = sigmoid(X*theta);
-
-cost = (1/m) * sum(-y.*log(prediction) - (1-y).*(log(1 - prediction))) + (lambda*(1/2)*(1/m)*(sum(theta(2:end, 1).^2)));
-
-end
